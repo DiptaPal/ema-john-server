09b65d097d931a34aa64cfc47e1ec85e1a7c507a
Dipta Pal <diptapal1998@gmail.com>
2023-09-24 13:51:16 +0600

    server updated

--- index.js
+++ index.js
@@ -12,39 +12,45 @@ app.use(express.json())
 
 
 
-// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.xm5hqxk.mongodb.net/?retryWrites=true&w=majority`;
-// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.xm5hqxk.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    }
+  });
 
-// async function run() {
-//     try{
-//         const productCollection = client.db('emaJohn').collection('products')
+async function run() {
+    try{
+        const productCollection = client.db('emaJohn').collection('products')
 
-//         app.get('/products', async(req, res) =>{
-//             const page = parseInt(req.query.page);
-//             const size = parseInt(req.query.size);
-//             console.log(page, size);
-//             const query = {}
-//             const cursor = productCollection.find(query);
-//             const products = await cursor.skip(page*size).limit(size).toArray();
-//             const count = await productCollection.estimatedDocumentCount();
-//             res.send({count, products});
-//         });
+        app.get('/products', async(req, res) =>{
+            const page = parseInt(req.query.page);
+            const size = parseInt(req.query.size);
+            console.log(page, size);
+            const query = {}
+            const cursor = productCollection.find(query);
+            const products = await cursor.skip(page*size).limit(size).toArray();
+            const count = await productCollection.estimatedDocumentCount();
+            res.send({count, products});
+        });
 
-//         app.post('/productsByIds', async(req, res) =>{
-//             const ids = req.body;
-//             const objectIds = ids.map(id => ObjectId(id))
-//             const query = {_id: {$in: objectIds}};
-//             const cursor = productCollection.find(query);
-//             const products = await cursor.toArray();
-//             res.send(products);
-//         })
+        app.post('/productsByIds', async(req, res) =>{
+            const ids = req.body;
+            const objectIds = ids.map(id => ObjectId(id))
+            const query = {_id: {$in: objectIds}};
+            const cursor = productCollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products);
+        })
 
-//     }
-//     finally {
-//         //await client.close();
-//     }
-// }
-// run().catch(error => console.log(error.message))
+    }
+    finally {
+        //await client.close();
+    }
+}
+run().catch(error => console.log(error.message))
 
 
 
